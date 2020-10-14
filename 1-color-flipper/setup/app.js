e23cc0e570ca753a42ea26fda1e9dbf873aecdb6
deanlauvina7 <deanlauvina7@gmail.com>
2020-10-14 11:53:33 -0500

    Color flipper project done

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -6,10 +6,11 @@ const color = document.querySelector('.color');
 btn.addEventListener('click', function name(params) {
   // Get random number between 0 and 3 
   const randomNumber = getRandomNumber();
+  console.log(randomNumber);
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
 });
 
-function getRandomNumber(params) {
-  return Math.random();
+function getRandomNumber() {
+  return Math.floor( Math.random() * colors.length);
 }
