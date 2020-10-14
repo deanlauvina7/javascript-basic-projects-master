c2f1db69d596352a682102957e064d5cc34cf86b
deanlauvina7 <deanlauvina7@gmail.com>
2020-10-14 12:32:05 -0500

    Counter project finished

---
+++ 2-counter/setup/app.js
@@ -0,0 +1,25 @@
+// Set initial count
+let count = 0;
+
+// Select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains('decrease')) {
+      count--;
+    } else if (styles.contains('increase')) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = 'green';
+    } else if (count < 0) {
+      value.style.color = 'red';
+    } else (count === 0)
+      value.textContent = count;
+  });
+});
