f79c17347f40127fc00d2c5bbfaeda13bbfb9820
deanlauvina7 <deanlauvina7@gmail.com>
2020-10-15 07:29:21 -0500

    Finished the q and a section

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,2 +1,10 @@
 //using selectors inside the element
+const btns = document.querySelectorAll('.question-btn');
 // traversing the dom
+
+btns.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    const question = e.currentTarget.parentElement.parentElement;
+    question.classList.toggle('show-text');
+  });
+});
