33b819ebdb9118c7fa33b86f6e9c7da725733c5b
deanlauvina7 <deanlauvina7@gmail.com>
2020-10-16 20:27:41 -0500

    Adjusting the scroll feature

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -4,10 +4,49 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear();
 
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
 
+navToggle.addEventListener('click', () => {
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
 // ********** fixed navbar ************
+const navBar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link');
 
+window.addEventListener('scroll', () => {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navBar.getBoundingClientRect().height;
+
+  if (scrollHeight > navHeight) {
+    navBar.classList.add('fixed-nav');
+  }
+
+  if (scrollHeight > 500) {
+    topLink.classList.add('show-link');
+  } else {
+    topLink.classList.remove('show-link');
+  }
+});
 // ********** smooth scroll ************
 // select links
+
+const scrollLinks = document.querySelector(',scroll-link');
+
+scrollLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+  });
+});
